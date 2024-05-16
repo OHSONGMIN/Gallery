0ada01d0496d85e3c8b1e701cd0fb193dddf56d8
OHSONGMIN <dhthdals@gmail.com>
2024-05-16 12:41:16 +0900

    Frontend - DB에서 받은 데이터(Object) 입력

--- Frontend/src/components/Card.vue
+++ Frontend/src/components/Card.vue
@@ -1,19 +1,14 @@
 <template>
   <div class="card shadow-sm">
-    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg"
-         role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice"
-         focusable="false"><title>Placeholder</title>
-      <rect width="100%" height="100%" fill="#55595c"/>
-      <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-    </svg>
+    <span class="img" :style="{backgroundImage: `url(${item.imgPath})`}" />
+<!--    <img :src ="item.imgPath" /> -->
     <div class="card-body">
-      <p class="card-text">{{ item }}</p>
+      <p class="card-text">{{ item.name }}</p>
       <div class="d-flex justify-content-between align-items-center">
-        <div class="btn-group">
-          <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-        </div>
-        <small class="text-body-secondary">9 mins</small>
+        <button class="btn btn-primary">구입하기</button>
+        <small class="text-body-secondary">
+          {{ item.price }}원
+        </small>
       </div>
     </div>
   </div>
@@ -22,11 +17,17 @@
 export default {
   name: "Card",
   props: {
-    item: String
+    item: Object
   }
 }
 </script>
 
 <style scoped>
-
+.card .img{
+  display:inline-block;
+  width:100%;
+  height:250px;
+  background-size: cover;
+  background-position: center;
+}
 </style>
