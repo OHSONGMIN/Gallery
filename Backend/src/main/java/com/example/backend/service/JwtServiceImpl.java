c941e7ac4a8838f6a1cf3fbc31366f98f9c48022
OHSONGMIN <dhthdals@gmail.com>
2024-05-17 22:43:35 +0900

    Backend - 로그인 체크 로직 보안
    JwtService를 통해 토큰 생성 및 클레임 추출

--- Backend/src/main/java/com/example/backend/service/JwtServiceImpl.java
+++ Backend/src/main/java/com/example/backend/service/JwtServiceImpl.java
@@ -1,8 +1,7 @@
 package com.example.backend.service;
 
-import io.jsonwebtoken.JwtBuilder;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Service;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
@@ -11,16 +10,17 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+@Service("jwtService")
 public class JwtServiceImpl implements JwtService {
 
     private String secretKey = "dhaiisdjhk!@#2135d@@@jdjskal%%%skdj123155djkdjl2123jkd0e80";
 
     @Override
     public String getToken(String key, Object value) {
-/*
- * 입력 받은 key와 value를 secretKey를 이용해서
- * 만들어주는 메서드
- */
+        /*
+         * 입력 받은 key와 value를 secretKey를 이용해서
+         * 만들어주는 메서드
+         */
         Date expTime = new Date();
         expTime.setTime(expTime.getTime() + 1000 * 60 * 5);
         //Jave 8까지만 지원
@@ -42,5 +42,25 @@ public class JwtServiceImpl implements JwtService {
                 .signWith(signKey, SignatureAlgorithm.HS256);
 
         return builder.compact();
+    }
+
+    @Override
+    public Claims getClaims(String token) {
+        if (token != null && !"".equals(token)) {
+            try {
+                String base64EncodedSecretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+                byte[] secretByteKey = Base64.getDecoder().decode(base64EncodedSecretKey);
+                Key signKey = new SecretKeySpec(secretByteKey, SignatureAlgorithm.HS256.getJcaName());
+
+                Claims claims = Jwts.parserBuilder().setSigningKey(signKey).build().parseClaimsJws(token).getBody();
+                return claims;
+            } catch (ExpiredJwtException e) {
+                //만료됨
+            } catch (JwtException e) {
+                //유효하지 않음
+            }
+        }
+
+        return null;
     }
 }
