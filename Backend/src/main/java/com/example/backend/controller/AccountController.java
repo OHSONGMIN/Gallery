c54cf7d989a77bb32237ade34744e581d7580a95
OHSONGMIN <dhthdals@gmail.com>
2024-05-17 16:25:46 +0900

    Backend - id값을 token화 & cookie에 넣어서 전달
    로그인 요청이 오고
    member 값이 null이 아니면
    id를 token화 해서 cookie에 넣은 다음에 응답 값에 추가하는 것
    
    Jwt로 로그인 보안

--- Backend/src/main/java/com/example/backend/controller/AccountController.java
+++ Backend/src/main/java/com/example/backend/controller/AccountController.java
@@ -4,8 +4,13 @@ import com.example.backend.entity.Item;
 import com.example.backend.entity.Member;
 import com.example.backend.repository.ItemRepository;
 import com.example.backend.repository.MemberRepository;
+import com.example.backend.service.JwtService;
+import com.example.backend.service.JwtServiceImpl;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,13 +27,25 @@ public class AccountController {
     MemberRepository memberRepository;
 
     @PostMapping("/api/account/login")
-    public int login(@RequestBody Map<String, String> params) {
+    public ResponseEntity login(@RequestBody Map<String, String> params,
+                                HttpServletResponse res) {
         Member member = memberRepository.findByEmailAndPassword(params.get("email"), params.get("password"));
 
-        if(member != null) {
-            return member.getId();
+        if (member != null) {
+            JwtService jwtService = new JwtServiceImpl();
+            int id = member.getId();
+            String token = jwtService.getToken("id", id);
+
+            Cookie cookie = new Cookie("token", token);
+            cookie.setHttpOnly(true); //JavaScript로 접근할 수 없도록
+            cookie.setPath("/");
+
+            res.addCookie(cookie); //cookie값 추가
+            System.out.println("id 맞나 " + id);
+            //return ResponseEntity.ok().build(); //라고만 하면 로그인 해도 로그아웃으로 변하지 않은....
+            return new ResponseEntity<>(id, HttpStatus.OK); //라고 하면 응답 값으로 id값을 줄 수 있음....
         }
-        
+
         //로그인 실패했을 때
         //return 0;
         throw new ResponseStatusException(HttpStatus.NOT_FOUND);
