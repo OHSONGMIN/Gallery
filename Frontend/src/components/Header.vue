60fad6f38b77abaf9eba9068766eacc95bc42284
OHSONGMIN <dhthdals@gmail.com>
2024-05-21 00:28:25 +0900

    Frontend - 로그인 시 장바구니 아이콘이 보이도록 수정 로그아웃 시 쿠키 내역 삭제하기 위해 "/api/account/logout"으로 연결

--- Frontend/src/components/Header.vue
+++ Frontend/src/components/Header.vue
@@ -32,7 +32,7 @@
           </svg>
           <strong>Gallery</strong>
         </router-link>
-        <router-link to="/cart" class="cart">
+        <router-link to="/cart" class="cart" v-if="$store.state.account.id">
           <i class="fa fa-shopping-cart" aria-hidden="true"></i>
         </router-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader"
@@ -47,14 +47,17 @@
 <script>
 import router from "@/scripts/router";
 import store from "@/scripts/store";
+import axios from "axios";
 
 export default {
   name: 'Header',
   setup() {
     const logout = () => {
-      store.commit("setAccount", 0);
-      sessionStorage.removeItem("id"); //sessionStorage의 id값 지우기
-      router.push({path: "/"});
+      axios.post("/api/account/logout").then(() => {
+        store.commit("setAccount", 0);
+        router.push({path: "/"});
+      })
+      //sessionStorage.removeItem("id"); //sessionStorage의 id값 지우기
     }
 
     return {logout}
@@ -68,4 +71,9 @@ header .navbar .cart {
   margin-left: auto;
   color: #fff;
 }
+
+header ul li a {
+  cursor: pointer;
+}
+
 </style>
