d9c2865a1a0ee8f09f0bc92e7602b1bd9db6edce
OHSONGMIN <dhthdals@gmail.com>
2024-05-21 00:26:46 +0900

    Backend - 주문내역 역순으로 출력
    findByMemberIdOrderByDesc(int memberId)

--- Backend/src/main/java/com/example/backend/repository/OrderRepository.java
+++ Backend/src/main/java/com/example/backend/repository/OrderRepository.java
@@ -3,7 +3,11 @@ package com.example.backend.repository;
 import com.example.backend.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 
+    //주문내역을 역순으로 출력하기 위해
+    List<Order> findByMemberIdOrderByIdDesc(int memberId);
 
 }
