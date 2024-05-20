cc94bca33c059da270d1c3a00e8c14384146656e
OHSONGMIN <dhthdals@gmail.com>
2024-05-21 00:30:54 +0900

    Backend - OrderController
    주문완료 되면 장바구니에서 삭제되도록 코드 및 @Transactional 추가

--- Backend/src/main/java/com/example/backend/controller/OrderController.java
+++ Backend/src/main/java/com/example/backend/controller/OrderController.java
@@ -2,8 +2,10 @@ package com.example.backend.controller;
 
 import com.example.backend.dto.OrderDto;
 import com.example.backend.entity.Order;
+import com.example.backend.repository.CartRepository;
 import com.example.backend.repository.OrderRepository;
 import com.example.backend.service.JwtServiceImpl;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,9 @@ import java.util.List;
 
 @RestController
 public class OrderController {
+
+    @Autowired
+    CartRepository cartRepository;
 
     @Autowired
     OrderRepository orderRepository;
@@ -29,11 +34,15 @@ public class OrderController {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
         }
 
-        List<Order> orders = orderRepository.findAll();
+        //List<Order> orders = orderRepository.findAll();
+        //역순으로 출력하기 위해 수정
+        int memberId = jwtService.getId(token);
+        List<Order> orders = orderRepository.findByMemberIdOrderByIdDesc(memberId);
 
         return new ResponseEntity<>(orders, HttpStatus.OK);
     }
 
+    @Transactional
     @PostMapping("/api/orders")
     public ResponseEntity pushOrder(
             @RequestBody OrderDto dto,
@@ -43,8 +52,10 @@ public class OrderController {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
         }
 
+        int memberId = jwtService.getId(token);
         Order newOrder = new Order();
-        newOrder.setMemberId(jwtService.getId(token));
+
+        newOrder.setMemberId(memberId);
         newOrder.setName(dto.getName()); //비어있을수도 있으니 유효성 검사 해주는 게 좋음
         newOrder.setAddress(dto.getAddress());
         newOrder.setPayment(dto.getPayment());
@@ -52,6 +63,7 @@ public class OrderController {
         newOrder.setItems(dto.getItems());
 
         orderRepository.save(newOrder);
+        cartRepository.deleteByMemberId(memberId);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
